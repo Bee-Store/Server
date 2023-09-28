5b26d1b46f92cf4432dfa4c31c4261de8ea4c4dc
Ian Kamau <kian99564@gmail.com>
2023-09-28 15:38:00 +0300

    Server setup

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Server
+
+- Clone
+
+        git clone git@github.com:Bee-Store/Server.git
+        cd Server
+        yarn
---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Database Connected succssfully"))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server listening on ${process.env.PORT}`);
+});
