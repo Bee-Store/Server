d6a1cca8697a89988fe6504d63052f4415f6f1f4
Ian Kamau <kian99564@gmail.com>
2023-09-29 10:33:24 +0300

    Fixing some bugs

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -1,14 +1,27 @@
+const User = require("./../models/User.model");
+const jwt = require("jsonwebtoken");
+const helpers = require("./../helpers/helpers");
+
 class UserController {
   constructor() {}
 
-  async;
-
-  getStatus(req, res) {
+  async Register(req, res) {
     try {
-      res.status(200).json({
-        status: 200,
-        message: "Route in great condition",
+      if (req.body.password !== req.body.password_confirmation) {
+        res.status(500).json({
+          message: "Password or email is incorrect",
+        });
+      }
+
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: await helpers.generateSecurePassword(req.body.password),
       });
+
+      const savedUser = await newUser.save()
+
+      res.status(200).json({ message: savedUser });
     } catch (error) {
       res.status(500).json({
         status: 500,
---
+++ src/models/User.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: [6, "The password is not strong enough"],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamp: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
