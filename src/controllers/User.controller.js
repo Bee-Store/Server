a429493f60fabf7139e8a84ffdc8a74d83c418bd
Ian Kamau <kian99564@gmail.com>
2023-09-29 17:29:43 +0300

    Registration done

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -1,5 +1,6 @@
 const User = require("./../models/User.model");
 const jwt = require("jsonwebtoken");
+const CryptoJS = require("crypto-js");
 const helpers = require("./../helpers/helpers");
 
 class UserController {
@@ -16,12 +17,16 @@ class UserController {
       const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: await helpers.generateSecurePassword(req.body.password),
+        password: CryptoJS.AES.encrypt(
+          req.body.password,
+          process.env.PASS_SEC
+        ).toString(),
+        phoneNumber: req.body.phoneNumber,
       });
 
-      const savedUser = await newUser.save()
+      const savedUser = await newUser.save();
 
-      res.status(200).json({ message: savedUser });
+      res.status(200).json({ message: newUser });
     } catch (error) {
       res.status(500).json({
         status: 500,
