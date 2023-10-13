a57c15404279ca6489a02061e27ea30132b1a3da
Ian Kamau <kian99564@gmail.com>
2023-10-13 16:25:03 +0300

    Admin register route implementedc

--- src/controllers/Admin.controller.js
+++ src/controllers/Admin.controller.js
@@ -2,6 +2,7 @@ const Admin = require("../models/Admin.model");
 const jwt = require("jsonwebtoken");
 const ejs = require("ejs");
 const path = require("path");
+const CryptoJS = require("crypto-js");
 const Logger = require("../middlewares/loggers/logger");
 const generateRandomString = require("../helpers/GenerateRandom");
 const dotenv = require("dotenv");
@@ -12,7 +13,34 @@ class AdminController {
   constructor() {}
 
   async Register(req, res) {
-    Logger.info("Testing Admin Controller");
+    try {
+      if (req.body.password !== req.body.password_confirmation) {
+        res.status(500).json({
+          message: "Password or email is incorrect",
+        });
+      }
+
+      const newUser = new Admin({
+        username: req.body.username,
+        email: req.body.email,
+        password: CryptoJS.SHA256(
+          req.body.password,
+          process.env.PASS_SEC
+        ).toString(),
+      });
+
+      await newUser.save();
+
+      res
+        .status(200)
+        .json({user: newUser})
+    } catch (error) {
+      Logger.error(error)
+      res.status(500).json({
+        status: 500,
+        message: "Server error",
+      });
+    }
   }
 }
 
