02a68330483f7624322a2b0abbbd3d77c7b9e5d3
Ian Kamau <kian99564@gmail.com>
2023-10-20 23:41:55 +0300

    Generating pdf from cart

--- src/documents/index.js
+++ src/documents/index.js
@@ -1,4 +1,23 @@
-module.exports = ({ name, email }) => {
+module.exports = (name, tempCart) => {
+  // Calculate the total amount
+  const totalAmount = tempCart.reduce(
+    (total, item) => total + item.product.price * item.quantity,
+    0
+  );
+
+  const productRows = tempCart
+    .map(
+      (item) => `
+    <tr>
+      <td>${item.quantity}</td>
+      <td>${item.product.name}</td>
+      <td>${item.product.price}</td>
+      <td>${item.product.price * item.quantity}</td>
+    </tr>
+  `
+    )
+    .join("");
+
   return `
     <!doctype html>
     <html>
@@ -12,37 +31,36 @@ module.exports = ({ name, email }) => {
                 padding: 2em 4em !important;
                 gap: 4em;
               }
-              .class-header{
+              .class-header {
                   display: flex;
                   justify-content: space-between;
               }
-              .receipt-info{
+              .receipt-info {
                   display: flex;
                   justify-content: space-between;
-                  /* gap: 3em; */
               }
-              .customer-info{
+              .customer-info {
                   display: flex;
                   flex-direction: column;
                   gap: 8px;
               }
-              .receipt-info > div{
+              .receipt-info > div {
                   display: flex;
                   flex-direction: column;
                   gap: 20px;
               }
-              .receipt-info > div:last-child{
+              .receipt-info > div:last-child {
                   direction: column;
                   gap: 8px;
               }
-              .receipt-info  div:last-child > div{
+              .receipt-info div:last-child > div {
                   display: flex;
                   gap: 10px;
               }
           </style>
        </head>
        <body>
-            <div class="receipt-wrapper">
+        <div class="receipt-wrapper">
           <div class="class-header">
             <span>BeeKissed ltd</span>
             <span>RECEIPT</span>
@@ -51,49 +69,40 @@ module.exports = ({ name, email }) => {
           <div class="receipt-info">
             <div>
               <span>Bill To</span>
-
               <div class="customer-info">
                 <!-- For customer name -->
                 <span>${name}</span>
                 <span>Umoja</span>
-                <span>Nairobi, kenya</span>
+                <span>Nairobi, Kenya</span>
               </div>
             </div>
             <div>
               <span>Ship To</span>
-
               <div class="customer-info">
                 <!-- For customer name -->
                 <span>${name}</span>
                 <span>Umoja</span>
-                <span>Nairobi, kenya</span>
+                <span>Nairobi, Kenya</span>
               </div>
             </div>
-
             <div>
               <div>
                 <span> Receipt# </span>
-
                 <!-- For Receipt ID -->
                 <span>US-001</span>
               </div>
-
               <div>
                 <span>Receipt Date</span>
-
                 <!-- For Receipt Date -->
                 <span>11/02/2019</span>
               </div>
-
               <div>
                 <span>Due Date</span>
-
                 <!-- For Receipt Due Date -->
                 <span>26/02/2019</span>
               </div>
             </div>
           </div>
-
           <!-- Table showing all purchased items -->
           <table>
             <thead>
@@ -105,33 +114,15 @@ module.exports = ({ name, email }) => {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>2</td>
-                <td>Fridge</td>
-                <td>15000</td>
-                <td>30000</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Iphone 12</td>
-                <td>10000</td>
-                <td>20000</td>
-              </tr>
-              <tr>
-                <td>5</td>
-                <td>Hp Pavillion</td>
-                <td>3000</td>
-                <td>150000</td>
-              </tr>
+              ${productRows}
               <tr>
                 <td></td>
                 <td></td>
                 <td>Total</td>
-                <td><strong> 302884</strong></td>
+                <td><strong>${totalAmount}</strong></td>
               </tr>
             </tbody>
           </table>
-
           <div>
             <h3>Terms & Conditions Apply</h3>
             <span>Thank you ${name}</span>
@@ -139,5 +130,5 @@ module.exports = ({ name, email }) => {
         </div>
        </body>
     </html>
-    `;
+  `;
 };
