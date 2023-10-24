7bcc3a48c236728a445189566d7851a57ba7c4ff
Ian Kamau <kian99564@gmail.com>
2023-10-24 09:53:56 +0300

    Created Order model

---
+++ src/models/Order.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const User = require("./User.model");
+const orderSchema = new mongoose.Schema(
+  {
+    customerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: User,
+    },
+    orderDetails: {
+      type: String,
+      required: false,
+    },
+    orderDate: {
+      type: Date,
+      required: true,
+    },
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Shipped", "Delivered", "Cancelled"],
+    },
+  },
+  { timestamp: true }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
